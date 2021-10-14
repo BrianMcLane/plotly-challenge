f2e880c6f15b1f5178ca8b855d526320db80206b
Brian <brianmclane@Brians-MBP.hitronhub.home>
2021-10-14 14:40:04 -0500

    homework

--- Plotly_Homework/Instructions/StarterCode/app.js
+++ Plotly_Homework/Instructions/StarterCode/app.js
@@ -2,13 +2,70 @@ d3.json("samples.json").then(function(data) {
   console.log(data)
 });
 
-function buildPlot() {
-  d3.json("samples.json").then(function(data) {
-    var name = data.name
+function buildCharts(sample) {
+  d3.json("samples.json").then((data) => {
+    var samples = data.samples;
+
+    var resultArray = samples.filter(sampleObj => sampleObj.id == sample);
+    var result = resultArray[0]
+
+    var values = result.sample_values;
+    var otu_ids = result.otu_ids; //slice(0,10).reverse();
+    var otu_labels = result.otu_labels;  //.slice(0,10).reverse()
+
+    var barData = {
+      x: values.slice(0,10).reverse(), 
+      y: otu_ids.slice(0, 10).map(otuID => `OTU ${otuID}`).reverse(),
+      text: otu_labels.slice(0, 10).reverse(),
+      type: "bar",
+      orientation: 'h'
+    };
+    
+    var databar = [barData];
+    
+    var layout = {
+      title: "Top 10 Bacteria Cultures"
+    };
+    
+    Plotly.newPlot("bar", databar, layout);
+
+
+    // Creating the bubble data
+    var bubbleData = {
+      x: otu_ids, 
+      y: values,
+      text: otu_labels,
+      mode: 'markers',
+      marker: {
+        size: values,
+        color: otu_ids,
+        colorscale: "Earth"
+    }
+
+    };
+    
+    var databubble = [bubbleData];
+    
+    var bubbleLayout = {
+      title: "Bacteria Cultures per Sample",
+      hovermode: "closest",
+      xaxis: {title: "OTU ID"},
+      margin : {t : 30}
+    };
+    
+    Plotly.newPlot("bubble", databubble, bubbleLayout);
+
+
+     
   })
 }
 
-
+// samples:[{
+//   "id": "940",
+//   "otu_ids": [1167, 2859, 482],
+//   "sample_values": [163, 126, 113],
+//   "otu_labels": ["Bacteria;Bacteroidetes;Bacteroidia;Bacteroidales;Porphyromonadaceae;Porphyromonas", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI;Peptoniphilus", "Bacteria",] 
+// }]
 
 
 
@@ -36,7 +93,8 @@ init();
 function optionChanged(newSample) {  
   // Get new data when changing name (id)  
   buildMetadata(newSample);  
-  //buildCharts(newSample);  
+  buildCharts(newSample);  
+  console.log(newSample)
 }
 
 //Demographics Panel 
